314601fcd59a6c3bd3ad71aef36d92651c6e8fcb
T4t1n <jaimiskytn@gmail.com>
2025-09-09 11:47:08 -0600

    9/09/2025

--- taco-cloud/src/main/java/com/tat1n/taco_cloud/DesignTacoController.java
+++ taco-cloud/src/main/java/com/tat1n/taco_cloud/DesignTacoController.java
@@ -4,10 +4,13 @@ package com.tat1n.taco_cloud;
 import com.tat1n.taco_cloud.Ingredient.Type;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -38,5 +41,40 @@ public class DesignTacoController {
           new Ingredient("SLSA", "Salsa", Type.SAUCE),
           new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
+        
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+    
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() {
+        return new TacoOrder();
+    }
+    
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+    
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+    
+    private Iterable<Ingredient> filterByType(
+        List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+    
+    @PostMapping
+    public String processTaco(Taco taco, 
+            @ModelAttribute TacoOrder tacoOrder) {
+        
     }
 }
