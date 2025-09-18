a083e8c7722abdaffa3ab77fa033e47de7c8fe0d
T4t1n <jaimiskytn@gmail.com>
2025-09-17 20:28:04 -0600

    Update 17/09/2025

--- taco-cloud/src/main/java/com/tat1n/taco_cloud/TacoOrder.java
+++ taco-cloud/src/main/java/com/tat1n/taco_cloud/TacoOrder.java
@@ -1,6 +1,7 @@
 
 package com.tat1n.taco_cloud;
 
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.ArrayList;
 import lombok.Data;
@@ -13,11 +14,17 @@ import lombok.Data;
 @Data
 public class TacoOrder {
     
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
+    @NotBlank(message="City is required")
     private String deliveryCity;
+    @NotBlank(message="State is required")
     private String deliveryState;
+    @NotBlank(message="Zip code is required")
     private String deliveryZip;
+    
     private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
